43f30a479896d21fbf5de6555150dbe25796f097
unknown <kauan.mazetorodrigues@gmail.com>
2025-09-25 19:45:46 -0300

    feat: style page clima

--- ClimaTop/src/app/app.ts
+++ ClimaTop/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('clima-time');
+  protected readonly title = signal('clima-top');
 }
