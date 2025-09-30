e328250e96c82f48f3f4390b62fbdbdeb7603666
unknown <kauan.mazetorodrigues@gmail.com>
2025-09-30 19:54:13 -0300

    feat: atualizando função

--- ClimaTop/src/app/service/open-weather.service.ts
+++ ClimaTop/src/app/service/open-weather.service.ts
@@ -1,18 +1,26 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WeatherResponse } from '../models/wheater-response.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenWeatherService {
 
-  apiKey = 'f39c61444538bcb34a7be173ade7f6ee';  
-  apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  private apiKey = 'f39c61444538bcb34a7be173ade7f6ee';
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  private http = inject(HttpClient);
+  
   constructor() { }
 
-  buscarInfoClimaCidadeAtual () {
+  buscarInfoClimaCidadeAtual (): Observable<WeatherResponse> {
 
     const urlCompleta = this.apiUrl + '?q=Dois%20Vizinhos&appid=' + this.apiKey + '&lang=pt_br&units=metrics';
     console.log(urlCompleta);
+
+    return this.http.get<WeatherResponse>(urlCompleta);
   }
 }
 
