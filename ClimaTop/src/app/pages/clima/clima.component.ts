032ba0cc7d74e0c0cb433ac41f35e360fdfb8042
unknown <kauan.mazetorodrigues@gmail.com>
2025-10-07 21:33:46 -0300

    feat: component minmax

--- ClimaTop/src/app/pages/clima/clima.component.ts
+++ ClimaTop/src/app/pages/clima/clima.component.ts
@@ -10,13 +10,14 @@ import { TemperaturaCidadeComponent } from "../../components/temperatura-cidade/
 import { CarregarCidadeComponent } from "../../components/carregar-cidade/carregar-cidade.component";
 import { OpenWeatherService } from '../../services/open-weather.service';
 import { ClimaCidadeComponent } from "../../components/clima-cidade/clima-cidade.component";
+import { MinmaxCidadeComponent } from "../../components/minmax-cidade/minmax-cidade.component";
 
 
 @Component({
   selector: 'app-clima',
   standalone: true,
   templateUrl: './clima.component.html',
-  imports: [DecimalPipe, NomeCidadeComponent, TemperaturaCidadeComponent, CarregarCidadeComponent, ClimaCidadeComponent],
+  imports: [DecimalPipe, NomeCidadeComponent, TemperaturaCidadeComponent, CarregarCidadeComponent, ClimaCidadeComponent, MinmaxCidadeComponent],
   styleUrls: ['./clima.component.scss']
 })
 
